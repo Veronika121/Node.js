52c6ae711290ea01458c145724b970a6cb14705d
Veronika <veroniqe@rambler.ru>
2021-04-09 19:29:02 +0200

    finished homework week1

--- week1/homework/exercises/3-web-server/server.js
+++ week1/homework/exercises/3-web-server/server.js
@@ -3,12 +3,26 @@
  */
 
 var http = require('http');
-
+const fs = require('fs');
 //create a server
 let server = http.createServer(function (req, res) {
-  // YOUR CODE GOES IN HERE
-	res.write('Hello World!'); // Sends a response back to the client
-	res.end(); // Ends the response
+  if (req.url === '/') {
+    const cont = fs.readFileSync('./index.html', 'utf8');
+    res.setHeader('Content-Type', 'text/html');
+    res.write(cont);
+  }
+  if (req.url === '/index.js') {
+    const cont = fs.readFileSync('./index.js', 'utf8');
+    res.setHeader('Content-Type', 'text/javascript');
+    res.write(cont);
+  }
+  if (req.url === '/style.css') {
+    const cont = fs.readFileSync('./style.css', 'utf8');
+    res.setHeader('Content-Type', 'text/css');
+    res.write(cont);
+  }
+
+  res.end(); // Ends the response
 });
 
-server.listen(3000); // The server starts to listen on port 3000
+server.listen(3000, () => console.log('server running')); // The server starts to listen on port 3000
---
+++ week1/homework/hackyourtemperature/server.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => {
+  res.send('hello from backend to frontend!');
+});
+app.listen(3000);
